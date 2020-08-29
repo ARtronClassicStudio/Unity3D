20f89eac76d01e80c5d30e137e1d61ee853c80ba
ARtronClassicStudio <artron.classic@gmail.com>
2020-08-29 15:15:09 +0300

    My Package
    Co-Authored-By: artronclassicstudio <68843488+artronclassicstudio@users.noreply.github.com>

---
+++ FrameCamera.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCamera : MonoBehaviour {
+
+	public int Frame;
+	public List<Vector3> Rotation;
+	public List<Vector3> Position;
+	public float TimePause = 5;
+	public bool Loop=false;
+	private bool one;
+
+	void Start () {
+
+		transform.position = Position[Frame];
+		transform.rotation = Quaternion.Euler (Rotation [Frame]);
+
+		if (Loop) {
+			StartCoroutine (ToPose ());
+		} else {
+			StartCoroutine (ToPoseOne ());
+		}
+	}
+
+	IEnumerator ToPoseOne(){
+		yield return new WaitForSeconds (TimePause);
+		transform.position = Position[Frame];
+		transform.rotation = Quaternion.Euler (Rotation [Frame]);
+		Frame++;
+		if (Frame >= Position.Count) {
+		
+		} else {
+			StartCoroutine (ToPoseOne ());
+		}
+	}
+	
+	IEnumerator ToPose(){
+		yield return new WaitForSeconds (TimePause);
+		transform.position = Position[Frame];
+		transform.rotation = Quaternion.Euler (Rotation [Frame]);
+		Frame++;
+
+		if (Frame >= Position.Count) {
+
+		
+		Frame = 0;
+			
+		}
+		StartCoroutine (ToPose ());
+	}
+
+}
+
+#if UNITY_EDITOR 
+[UnityEditor.CustomEditor(typeof(FrameCamera))]
+public class Button : UnityEditor.Editor{
+	public override void OnInspectorGUI ()
+	{
+		DrawDefaultInspector ();
+		var pose = (FrameCamera)target;
+
+		GUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Add Pose")) {
+			pose.Position.Add (pose.transform.position);
+			pose.Rotation.Add (pose.transform.rotation.eulerAngles);
+		}
+		if (GUILayout.Button ("Remove Pose")) {
+
+			pose.Position.Remove (pose.Position[0]);
+			pose.Rotation.Remove (pose.Rotation[0]);
+			
+			
+		}
+		GUILayout.EndHorizontal ();
+	}
+
+}
+
+
+#endif
